23cb5df76e40c3a3bf96121cd83b045e67e070d3
el-01 <quantumleo78@gmail.com>
2020-03-07 11:01:17 -0700

    New climbing Code

---
+++ src/main/kotlin/frc/robot/subsystems/ClimbSubsystem.kt
@@ -0,0 +1,22 @@
+/*----------------------------------------------------------------------------*/
+/* Copyright (c) 2018-2019 FIRST. All Rights Reserved.                        */
+/* Open Source Software - may be modified and shared by FRC teams. The code   */
+/* must be accompanied by the FIRST BSD license file in the root directory of */
+/* the project.                                                               */
+/*----------------------------------------------------------------------------*/
+
+package frc.robot.subsystems
+
+import edu.wpi.first.wpilibj2.command.SubsystemBase
+import edu.wpi.first.wpilibj.SpeedController
+
+class ClimbSubsystem(val climbMotor: SpeedController) : SubsystemBase() {
+    override fun periodic() {
+        climbMotor.set(0.0)
+    }
+
+    fun setClimber(speed: Double){
+        climbMotor.set(speed)
+    }
+
+}
